6daf42177cf64b38e4da78367176ac8550c24b09
Ben Hansen <bhbenjaminhansen@gmail.com>
2020-10-17 23:24:45 -0600

    added nuxt

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div id="index">
+        <h1>Hello WebGPU from Nuxt!</h1>
+        <!-- Canvas goes here! -->
+    </div>
+</template>
+<script>
+// import init from "../generated/nuxt-wgpu.js";
+// init();
+export default {
+}
+</script>
